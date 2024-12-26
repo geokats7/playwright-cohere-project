9ef01d6f88a960964d894f037ed6d5c8ede1e787
George Katsaros <katsaros@orfium.com>
2024-12-26 15:00:13 +0200

    fix: chat page locator

--- tests/page_objects/chat.py
+++ tests/page_objects/chat.py
@@ -5,6 +5,7 @@ class ChatPage:
         self.message_input = page.get_by_placeholder("Message...")
         self.send_button = page.get_by_role("button", name="")
         self.response_message = page.locator("xpath=//main/section/div/div[2]/div/div/div/div[1]/div[2]/div[2]/div/div/div[2]/div[1]/div[@data-component='MessageContent']/div/div/p")
+        self.file_chooser = self.page.locator("xpath=//*[@id=\"message-list\"]/div[2]/div/div/div[1]/input")
 
     def send_message(self, message):
         self.message_input.click()
@@ -12,6 +13,5 @@ class ChatPage:
         self.send_button.click()
 
     def upload_file(self, file_path):
-        file_chooser = self.page.locator("xpath=//*[@id=\"message-list\"]/div[2]/div/div/div[1]/input")
-        file_chooser.set_input_files(file_path)
-        return file_chooser
+        self.file_chooser.set_input_files(file_path)
+        return self.file_chooser
